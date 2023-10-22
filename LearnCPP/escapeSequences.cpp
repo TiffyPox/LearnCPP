8f3bf341e3043838e33076e66c9ece7f4286289c
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-10-22 16:16:20 -0400

    add escapeSequences

--- LearnCPP/escapeSequences.cpp
+++ LearnCPP/escapeSequences.cpp
@@ -4,5 +4,9 @@ int main()
 {
     std::cout << "Bonjour, je m'appelle Tiffany\n" << "\v...et je suis trente deux ans." << "\tDaniel est mon mari.";
 
+    char a = 84;
+
+    std::cout << "\x48\x49 " << a << "\127\111" << a << a << "\x45\x52" << std::endl;
+
     return 0;
 }
