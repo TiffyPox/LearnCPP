afee7a75b0851a46cc11c783d34cb179e7d5bf38
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-10-26 22:19:13 -0400

    add palindromeString

--- LearnCPP/palindromeString.cpp
+++ LearnCPP/palindromeString.cpp
@@ -4,22 +4,36 @@ int main()
 {
     std::cout << "Give me a word" << std::endl;
 
+    bool palindrome = true;
+
     std::string input;
 
-    std::cin >> input;
-
-    for (size_t i = 0; i < input.length(); i++)
+    while (getline(std::cin, input))
     {
-        auto char1 = input[i];
-        auto char2 = input[input.length() - 1 - i];
+        for (size_t i = 0; i < input.length(); i++)
+        {
+            auto char1 = input[i];
+            auto char2 = input[input.length() - 1 - i];
 
-        if (char1 != char2)
+            if (char1 != char2)
+            {
+                palindrome = false;
+            }
+            else
+            {
+                palindrome = true;
+            }
+        }
+
+        if (palindrome == false)
         {
             std::cout << "That word is not a palindrome" << std::endl;
-
-            return -1;
+        }
+        else
+        {
+            std::cout << "That word is a palindrome!" << std::endl;
         }
     }
-    
-    std::cout << "That word is a palindrome!" << std::endl;
+
+    return 0;
 }
