39f30c72ee3ba909c636d9a58881f938b57418b0
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-11-06 20:43:06 -0500

    update vectorEquality

--- LearnCPP/vectorEquality.cpp
+++ LearnCPP/vectorEquality.cpp
@@ -1,6 +1,29 @@
 #include <vector>
 #include <iostream>
 
+bool areEqual(const std::vector<int> &v1, const std::vector<int> &v2)
+{
+    if (v1.size() != v2.size())
+    {
+        std::cout << "Vectors are not the same size" << std::endl;
+
+        return false;
+    }
+
+    for (int i = 0; i <= v1.size(); i++)
+    {  
+        int first = v1[i];
+        int second = v2[i];
+
+        if (first != second)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     // Write a program to check two vectors for equality
@@ -15,6 +38,20 @@ int main()
     else
     {
         std::cout << "Vectors are equal" << std::endl;
+    }
+
+    // That was too easy so let's try this...
+
+    std::vector<int> v1 { 2, 4, 6, 8 };
+    std::vector<int> v2 { 3, 4, 5 };
+
+    if (areEqual(v1, v2))
+    {
+        std::cout << "Vectors are equal" << std::endl;
+    }
+    else
+    {
+        std::cout << "Vectors are not equal" << std::endl;
     }
 
     return 0;
