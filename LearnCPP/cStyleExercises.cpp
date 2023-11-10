c39e32035a3ea11e6b08a358eb105d9006d09764
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-11-09 22:47:05 -0500

    update cStyleExercises

--- LearnCPP/cStyleExercises.cpp
+++ LearnCPP/cStyleExercises.cpp
@@ -16,7 +16,6 @@ int main()
     }
 
     // Write a program to compare two strings
-
     std::string first = {"Hello Github"};
     std::string second = {"Hello Twitter"};
 
@@ -34,7 +33,6 @@ int main()
     }
 
     // Write a program to compare two c-style strings
-
     char p1[6] = {'M', 'a', 'r', 'i', 'o', '\0'};
     char p2[6] = {'L', 'u', 'i', 'g', 'i', '\0'};
 
@@ -57,6 +55,25 @@ int main()
             std::cout << p2 << " is greater than " << p1 << std::endl;
         }
     }
+
+    // Write a program to define three character arrays. Use the third one to hold the concatenation of the first two
+    // using strcpy and strcat to copy the two arrays into the third
+    char c1[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    char c2[7] = {' ', 'W', 'o', 'r', 'l', 'd', '\0'};
+    char full[255]{};
+
+    std::strcat(c1, c2);
+    std::strcpy(full, c1);
+
+    std::cout << full << std::endl;
+
+    // c_str indicates that the function returns a C-Style character string
+    // it returns a pointer to the beginning of a null-terminated character array that holds the same data as the characters in the string
+    std::string s("Tiffany");
+
+    const char *str = s.c_str();
+
+    std::cout << *str;
     
     return 0;
 }
