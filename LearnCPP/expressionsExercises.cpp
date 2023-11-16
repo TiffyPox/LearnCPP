57c3b152e87afd7d0a25f05f5b5d22a5958fcca1
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-11-15 22:20:18 -0500

    update expressionsExercises

--- LearnCPP/expressionsExercises.cpp
+++ LearnCPP/expressionsExercises.cpp
@@ -7,5 +7,27 @@ int main()
     int sum = 5 + 10 * 20/2;
     std::cout << sum << std::endl;
 
+    // bool values should not be used for computation
+    bool b = true;
+    bool b2 = -b; // also true
+
+    std::cout << b2;
+
+    // for most operators, operands of type bool are promoted to int
+    // the value of b is true (int value 1)
+    // that (promoted) value is negated, yielding -1
+    // the value -1 is converted back to bool and used to initialize b2
+    // this initializer is a nonzero value, which when converted to bool, is true
+
+    // paranthesize the following expression
+    std::cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 << std::endl;
+
+    std::cout << (((12 / 3) * 4) + (5 * 15)) + ((24 % 4) / 2) << std::endl;
+
+    int sumA = -30 * 3 + 21 / 5; // (-30 * 3 = -90) + (21 / 5 = 4.2) answer is -86
+    int sumB = -30 + 3 * 21 / 5; // -18
+    int sumC = -30 / 3 * 21 % 5; // 0
+    int sumD = -30 / 3 * 21 % 4; // -2
+
     return 0;
 }
