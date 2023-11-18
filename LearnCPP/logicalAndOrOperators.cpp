09875f7628338250213b9fb77ca5547df71d4f69
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-11-17 22:30:42 -0500

    add logicalAndOrOperators

---
+++ LearnCPP/logicalAndOrOperators.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // the logical AND and OR operators always evaluate their left operand before the right
+    // moreover, the right operand is evaluated if and only if the left operand does not detemine the result
+
+    std::string text = "Hello World How Are You?";
+
+    int index = 0;
+
+    // checks that index has not reached the end of its associated string
+    // it is guaranteed that the right operand won't be evaluated unless index is in range
+    index != text.size() && !isspace(text[index]);
+
+    for (const auto &s : text)
+    {
+        std::cout << s;
+
+        if (s != text.size() && !isspace(s))
+        {
+            std::cout << " ";
+        }
+        else
+        {
+            std::cout << std::endl;
+        }
+    }
+
+    return 0;
+}
