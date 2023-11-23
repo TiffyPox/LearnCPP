e8579d5dd244edc58a9a77875901fc04067fa072
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-11-22 21:24:38 -0500

    update memberAccessOperators

--- LearnCPP/memberAccessOperators.cpp
+++ LearnCPP/memberAccessOperators.cpp
@@ -10,9 +10,9 @@ int main()
     std::string s1 = "a string"; 
     std::string *p1 = &s1;
 
-    auto n = s1.size(); // run the size member of s1
-    n = (*p1).size(); // run size on the object to which p points
-    n = p1->size(); // equivalent to (*p).size()
+    auto n1 = s1.size(); // run the size member of s1
+    n1 = (*p1).size(); // run size on the object to which p points
+    n1 = p1->size(); // equivalent to (*p).size()
 
     // because dereference has a lower precedence than dot, we must paranthesize the dereference subexpression
     // *p.size(); // error: p is a pointer and has no member named size
@@ -22,25 +22,33 @@ int main()
     * however, p is a pointer, which has no members
     */
 
-   // exercises
-   std::vector<std::string> text {"Hello Twitter!"};
+    // exercises
+    std::vector<std::string> text1 {"Hello Twitter!"};
 
-   std::vector<std::string>::iterator iter;
+    std::vector<std::string> text2 {"How are you?"};
 
-   iter = text.begin();
+    std::vector<std::string>::iterator iter1;
 
-   if (iter != text.end() && !(*iter).empty())
-   {
-    std::cout << *iter++ << std::endl;
-   }
+    std::vector<std::string>::iterator iter2;
 
-   auto e = text.empty();
-   e = iter->empty();
+    iter1 = text1.begin();
+    iter2 = text2.begin();
 
-   if (!e)
-   {
-    std::cout << "String is not empty!" << std::endl;
-   }
-   
-   return 0;
+    if (iter1 != text1.end() && !(*iter1).empty())
+    {  
+        std::cout << *iter1++ << std::endl;
+    }
+
+    auto e = text1.empty();
+    auto f = text2.empty();
+    
+    e = iter1->empty();
+    f = iter2++->empty();
+
+    if (!e || !f)
+    {
+        std::cout << "String is not empty!" << std::endl;
+    }
+
+    return 0;
 }
