b268ee85999dca2651b4577c613b62f875df7563
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-12-05 20:52:41 -0500

    update sizeOfOperatorExercises

--- LearnCPP/sizeOfOperatorExercises.cpp
+++ LearnCPP/sizeOfOperatorExercises.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // print the amount of bytes in each built-in type
+    // write a program to print the size of each of the built in types
 
     bool a; // 1
     short b; // 2
@@ -24,6 +24,17 @@ int main()
     char32_t l; // 4
 
     std::cout << sizeof j << sizeof k << sizeof l << std::endl;
+
+    // predict the output of the following code
+
+    int x[10];
+    int *p = x;
+
+    std::cout << sizeof x << std::endl; // 40
+    std::cout << sizeof &p << std::endl; // 8
+
+    std::cout << sizeof(x)/sizeof(*x) << std::endl; // 10
+    std::cout << sizeof(p)/sizeof(*p) << std::endl; // 2
 
     return 0;
 }
