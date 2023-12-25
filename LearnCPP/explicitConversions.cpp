578ca492a5da9233906912f83a740af2e3771556
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2023-12-24 21:34:32 -0500

    update explicitConversions

--- LearnCPP/explicitConversions.cpp
+++ LearnCPP/explicitConversions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
@@ -19,8 +20,23 @@ int main()
     // we can use a static_cast to retrieve a pointer value that was stored in a void* pointer
 
     double d = 32;
-    void* p = &d;
-    double *dp = static_cast<double*>(p); // converts void* back to the original pointer type
+    void* p1 = &d;
+    double *dp = static_cast<double*>(p1); // converts void* back to the original pointer type
+
+    // const_cast changes only a low-level const in its operand
+    const char *pc;
+    char *p2 = const_cast<char*>(pc);
+
+    /*
+    * only a const_cast can be used to change the constness of an expression
+    * we can't use const_cast to change the type of an expression
+    */
+
+   const char *cp;
+   // error: char *q1 = static_cast<char*>(cp); (static_cast can't cast away const)
+
+   static_cast<std::string>(cp); // converts string literal to string
+   // error: const_cast<std::string>(cp); (const_cast only changes constness, NOT type)
 
     return 0;
 }
