0a7cfb03157196a1a04a2017be0bcefd034bc024
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2024-01-08 20:03:52 -0500

    update statements

--- LearnCPP/statements.cpp
+++ LearnCPP/statements.cpp
@@ -22,16 +22,42 @@ int main()
     * for example we might want to read an input stream, ignoring everything until we encounter a particular value:
     */
 
-   char sought = 63;
+    char sought = 63;
+    char c;
 
-   char s;
-
-    while (std::cin >> s && s != sought)
+    while (std::cin >> c && c != sought)
     {
         ; // null statement, if the value is found, exit the loop
     }
 
-    // Null statements SHOULD be commented so that anyone reading the code knows the statement was intentional
+    // null statements SHOULD be commented so that anyone reading the code knows the statement was intentional
+
+    // Compound Statements (Blocks)
+
+    /*
+    * a compound statement, usually referred to as a block, is a (possibly empty) sequence of statements
+    * and declarations surrounded by a pair of curly braces
+    */
+
+    int val = 0;
+    int sum = 0;
+
+    while (val <= 10)
+    {
+        sum += val; // assigns sum + val to sum
+        ++val; // add 1 to val
+    }
+
+    // by enclosing these statements in curly braces, we made them into a single (compound statement)
+
+    // we can also define an empty block by writing a pair of curlies with no statements
+    // an empty block is equivalent to a null statement:
+
+    int score = 10;
+    int i;
+
+    while (std::cin >> i && i != score)
+    { } // empty block
 
     return 0;
 }
