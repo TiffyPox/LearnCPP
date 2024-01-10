f071a497df33fe6bca1b87ff5a77949b7f1b431d
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2024-01-09 23:26:51 -0500

    update mergeSort

--- LearnCPP/mergeSort.cpp
+++ LearnCPP/mergeSort.cpp
@@ -6,7 +6,14 @@ void printVector(const std::vector<int>& v)
 {
     for (auto it = v.begin(); it != v.end(); ++it)
     {
-        std::cout << *it << " ";
+        if (it != v.end() - 1)
+        {
+            std::cout << *it << " ";
+        }
+        else
+        {
+            std::cout << *it << std::endl;
+        }
     }
 }
 
@@ -37,9 +44,51 @@ int main()
         n2.push_back(curr);
     }
 
-    std::cout << std::endl;
-
     printVector(n2);
+
+    std::vector<int> firstHalf = {};
+    std::vector<int> secondHalf = {};
+
+    // get first half
+    for (auto i = n1.begin(); i < n1.end() - 2; ++i)
+    {
+        int curr = *i;
+
+        firstHalf.push_back(curr);
+    }
+
+    // get second half
+    for (auto i = n1.begin() + 2; i < n1.end(); ++i)
+    {
+        int curr = *i;
+
+        secondHalf.push_back(curr);
+    }
+
+    printVector(firstHalf);
+    printVector(secondHalf);
+
+    // sort first half
+    // TO DO: make this into a reusable function
+    int firstHalfLeft = firstHalf[0];
+    int secondHalfLeft = secondHalf[0];
+
+    std::vector<int> temp = {};
+
+    if (firstHalfLeft > secondHalfLeft)
+    {
+        temp.push_back(secondHalfLeft);
+    }
+
+    firstHalfLeft = firstHalf[0];
+    secondHalfLeft = secondHalf[1];
+
+    if (firstHalfLeft > secondHalfLeft)
+    {
+        temp.push_back(secondHalfLeft);
+    }
+
+    printVector(temp);
 
     // merge sorted numbers
 
