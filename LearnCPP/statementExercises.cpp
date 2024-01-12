e288c3bb30373d02665d06e855d743db7349dab6
tiffypox <tiffanyjaynedavies@hotmail.co.uk>
2024-01-11 21:40:32 -0500

    add statementExercises

--- LearnCPP/statementExercises.cpp
+++ LearnCPP/statementExercises.cpp
@@ -2,8 +2,6 @@
 
 int main()
 {
-    // rewrite the following code using the comma operator so that it no longer requires a block:
-
     int sum = 0;
     int val = 1;
 
@@ -14,12 +12,15 @@ int main()
     }
 
     std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    
+    // rewrite the code above using the comma operator so that it no longer requires a block:
 
     int num = 0;
 
-    for (int i = 0; i <= 10; i++, num += i);
+    for (int i = 1; i <= 10; num += i, ++i);
 
-    std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+    std::cout << "Sum of 1 to 10 inclusive is " << num << std::endl;                                
+    
 
     return 0;
 }
