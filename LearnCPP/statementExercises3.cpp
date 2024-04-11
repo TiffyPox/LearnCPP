51d757a3c73d5c9e1ddce7e6a87e7f42b54bdd86
Tiffany <tiffanyjaynedavies@hotmail.co.uk>
2024-04-10 20:10:49 -0400

    add statementExercises2

---
+++ LearnCPP/statementExercises3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    // given two vectors of ints, write a program to determine whether one vector is a prefix of the other
+
+    std::vector<int> v1 = {0, 1, 1, 2};
+    std::vector<int> v2 = {0, 1, 1, 2, 3, 5, 8};
+
+    return 0;
+}
